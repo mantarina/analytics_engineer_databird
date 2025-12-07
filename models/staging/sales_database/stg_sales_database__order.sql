acea68e48e4a36e1c5fe0b6fd200e814fc025cf8
Marine Verdon <marine.verdon@keyrus.com>
2025-12-07 13:57:34 +0100

    fix:add-suffix-column-order

--- models/staging/sales_database/stg_sales_database__order.sql
+++ models/staging/sales_database/stg_sales_database__order.sql
@@ -3,9 +3,9 @@ select
     1 AS colonne_1,
     user_name as user_id,
     order_status,
-    DATETIME(order_date, "Europe/Paris") AS order_created_at,
-    DATETIME(order_approved_date, "Europe/Paris") AS order_approved_at,
-    DATETIME(pickup_date, "Europe/Paris") AS picked_up_at,
-    DATETIME(delivered_date, "Europe/Paris") AS delivered_at,
-    DATETIME(estimated_time_delivery, "Europe/Paris") AS estimated_time_delivery
+    DATETIME(order_date, "Europe/Paris") AS order_created_at_x,
+    DATETIME(order_approved_date, "Europe/Paris") AS order_approved_at_x,
+    DATETIME(pickup_date, "Europe/Paris") AS picked_up_at_x,
+    DATETIME(delivered_date, "Europe/Paris") AS delivered_at_x,
+    DATETIME(estimated_time_delivery, "Europe/Paris") AS estimated_time_delivery_x
 from {{ source('sales_database', 'order') }}
